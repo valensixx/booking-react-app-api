46224a3a766866ab780c4e67d02edfc54ad6b42d
valensixx <valensixx@gmail.com>
2023-07-28 13:47:14 +0300

    working on registration

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const User = require('./models/User.js');
 require('dotenv').config();
-const app = express();  
+const app = express();
 
 const bcryptSalt = bcrypt.genSaltSync(10);
 
@@ -16,17 +16,22 @@ app.use(cors({
 
 await mongoose.connect(procrss.env.MONGO_URL);
 
-app.get('/test', (req,res)=>{
+app.get('/test', (req, res) => {
     res.json('test ok');
 });
 
 app.post('/register', async (req, res) => {
-    const {name, email, password} = req.body;
-    const userDoc = await User.create({
-        name, email, password:bcrypt.hashSync(password, bcryptSalt),
-    });
+    const { name, email, password } = req.body;
 
-    res.json(userDoc);
+    try {
+        const userDoc = await User.create({
+            name, email, password: bcrypt.hashSync(password, bcryptSalt),
+        });
+        res.json(userDoc);
+    } catch (e) {
+        res.status(422).json(e);
+    }
+
 });
 
 app.listen(4000);

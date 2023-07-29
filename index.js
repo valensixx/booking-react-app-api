bac6df14e79e35f3b34037db7f97f185c25c3a9e
valensixx <valensixx@gmail.com>
2023-07-29 17:50:39 +0300

    login

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.post('/login', async (req,res)=>{
         if(passOk){
             jwt.sign({email:userDoc.email, id:userDoc._id}, jwtSecret, {},(err,token)=>{
                 if (err) throw err;
-                res.cookie('token', token).json('password ok');
+                res.cookie('token', token).json(userDoc);
             });
         }else{
             res.status(422).json('password not ok')
